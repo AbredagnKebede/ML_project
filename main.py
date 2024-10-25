0d27e9989cd99db7eec8f298cec6761b51d42bdd
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-10-26 02:13:45 +0300

    go

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 #dataset manipulation
 my_dataset = pd.read_csv('C:\\Users\\hana\\Desktop\\practice programming\\Ml_diabates\\diabetes.csv')
+x = my_dataset.drop(columns= "Outcome", axis= 1)
+y = my_dataset["Outcome"]
+scaler = StandardScaler()
+scaler.fit(x)
+standardized_data = scaler.transform(x)
+print(standardized_data)
