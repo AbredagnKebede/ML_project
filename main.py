db93e6a75ed4a0d118f6696d235bc654fec7b09f
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-10-26 01:14:32 +0300

    go

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 #dataset manipulation
-my_dataset = pd.read_csv('C:\Users\hana\Desktop\practice programming\Ml_diabates\diabetes.csv')
+my_dataset = pd.read_csv('C:\\Users\\hana\\Desktop\\practice programming\\Ml_diabates\\diabetes.csv')
